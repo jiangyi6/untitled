c24f5eb18e3a013a56820bc436f4a7970f7055ab
sone <sone@qq.com>
2023-01-14 18:27:09 +0800

    hot-fix commit

--- src/main/java/Git/GItTest.java
+++ src/main/java/Git/GItTest.java
@@ -4,5 +4,7 @@ public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello GitTest");
         System.out.println("hello GitTest2");
-        System.out.println("hello GitTest3");    }
+        System.out.println("hello GitTest3");
+        System.out.println("hot-fix commit");
+    }
 }
