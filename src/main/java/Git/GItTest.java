f3742dc407041b65c4c45f2c4ee2b97c5df9ab31
sone <sone@qq.com>
2023-01-14 18:14:04 +0800

    master commit2

--- src/main/java/Git/GItTest.java
+++ src/main/java/Git/GItTest.java
@@ -3,5 +3,6 @@ package Git;
 public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello GitTest");
+        System.out.println("hello GitTest2");
     }
 }
